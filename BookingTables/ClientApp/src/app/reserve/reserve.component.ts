8a077bbbe1cd8015bad1b49be8f165141afe72b3
Yurii Kniazyk <y.kniazyk@gmail.com>
2020-08-18 22:55:55 +0300

    add orders

--- BookingTables/ClientApp/src/app/reserve/reserve.component.ts
+++ BookingTables/ClientApp/src/app/reserve/reserve.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-reserve-component',
@@ -7,18 +8,32 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./reserve.component.css']
 })
 export class ReserveComponent {
-  public orders: Reserve[];
+  public order = {
+    dateStart: '',
+    timeOfBooking: 0,
+    countUser: 0,
+    tableId: ''
+  };
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Reserve[]>(baseUrl + 'api/table').subscribe(result => {
-      this.orders = result;
-    }, error => console.error(error));
+  public baseUrl: string;
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private route: ActivatedRoute, private router: Router) {
+    this.route.params.subscribe(params => {
+      this.order.tableId = params['uuid'];
+    });
+
+    this.baseUrl = baseUrl;
   }
-}
 
-interface Reserve {
-  id: string;
-  dateStart: Date;
-  timeOfBooking: number;
-  countUser: number;
+  reserve() {
+    this.http.post(this.baseUrl + 'api/orders', this.order).subscribe((data) => {
+      this.order = {
+        dateStart: '',
+        timeOfBooking: 0,
+        countUser: 0,
+        tableId: ''
+      };
+    }, error => console.error(error));
+    console.log('user', this.order);
+  }
 }
